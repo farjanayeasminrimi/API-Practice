76cddb17051d83345222fe09963469e9c8a3e25d
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-03-04 18:30:37 +0600

    Practicing JSON

--- script.js
+++ script.js
@@ -3,10 +3,18 @@ const person = {
   age: 19,
   isMarried: false,
 };
-console.log(person, typeof person);
+// console.log(person, typeof person);
 
 const personStr = JSON.stringify(person);
-console.log(personStr, typeof personStr);
+// console.log(personStr, typeof personStr);
 
 const convertObj = JSON.parse(personStr);
-console.log(convertObj, typeof convertObj);
+// console.log(convertObj, typeof convertObj);
+
+// fetch()
+
+const loadFun = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
